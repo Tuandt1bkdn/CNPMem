de4d2459f594f9b3764939f92b71995d58fb46e5
Tuan <tuanqxktk5@outlook.com>
2022-12-06 16:42:05 +0700

    getDoctor

--- API/controller/medical_controller.js
+++ API/controller/medical_controller.js
@@ -1,92 +1,96 @@
-
-
-const mysql = require ("mysql");
+const mysql = require("mysql");
 const { response } = require("../../server.js");
 const Usersmodels = require("../model/users.models");
-const database = require ("../connectdatabase.js");
-const jwt = require("jsonwebtoken")
+const database = require("../connectdatabase.js");
+const jwt = require("jsonwebtoken");
 //const ACCESS_TOKEN_SECRET = jwtabc
 // const dotenv = require("/Workspace/Ex1/.env")
 // dotenv.config();
 
-
 module.exports = {
-    getAccount : function(req, res){
-        var sql = 'SELECT * FROM biqtzwqiihjmw2npadtd.Account '
-    database.query(sql , (err, response) => {
-        if (err) {
-            console.log("khong the lay du lieu");
-        }
-        res.json(response);
-    })
-},
-    getMedicine : function(req, res){
-        var sql = 'SELECT * FROM biqtzwqiihjmw2npadtd.Medicine '
-    database.query(sql , (err, response) => {
-        if (err) {
+  getAccount: function (req, res) {
+    var sql = "SELECT * FROM biqtzwqiihjmw2npadtd.Account ";
+    database.query(sql, (err, response) => {
+      if (err) {
         console.log("khong the lay du lieu");
-        }
-        res.json(response);
-        })
-},
-getuser : function(req, res){
-    var sql = 'SELECT * FROM biqtzwqiihjmw2npadtd.Patient '
-database.query(sql , (err, response) => {
-    if (err) {
-    console.log("khong the lay du lieu");
-    }
+      }
+      res.json(response);
+    });
+  },
+  getMedicine: function (req, res) {
+    var sql = "SELECT * FROM biqtzwqiihjmw2npadtd.Medicine ";
+    database.query(sql, (err, response) => {
+      if (err) {
+        console.log("khong the lay du lieu");
+      }
+      res.json(response);
+    });
+  },
+  getuser: function (req, res) {
+    var sql = "SELECT * FROM biqtzwqiihjmw2npadtd.Patient ";
+    database.query(sql, (err, response) => {
+      if (err) {
+        console.log("khong the lay du lieu");
+      }
+      res.json(response);
+    });
+  },
+  update: function (req, res) {
+    var data = req.body;
+    console.log(data);
+    var sql = "UPDATE account SET Role = '3' where UserName = 'Huong'";
+    console.log("HFfhsf");
+    database.query(sql, [data, "Role_ID"], (err, response) => {
+      if (err) throw err;
+      console.log("message: 'Update success!'");
+    });
     res.json(response);
-    })
-},
-  update : function(req, res) {
-        var data = req.body
-        console.log(data)
-    var sql = 'UPDATE account SET Role = \'3\' where UserName = \'Huong\''
-        console.log('HFfhsf');
-        database.query(sql, [data, 'Role_ID' ], (err, response) => {
-            if (err) throw err
-            console.log("message: 'Update success!'");
-    })
-    res.json(response);
   },
-  postAccount : function(req, res) {
-      var Account = req.body
-        
-      
-      console.log(Account)
+  postAccount: function (req, res) {
+    var Account = req.body;
 
-        var sql = `INSERT INTO biqtzwqiihjmw2npadtd.Account(UserName, Password, Role, IsActive, Role_ID)     VALUES("${Account.UserName}", "${Account.Password}", "${Account.Role}", "${Account.IsActive}", "${Account.Role_ID}")`
-        database.query(sql, (err, res)=> {
-            if (err)  throw err ;
-            res.json(Account)
-        })
-  },
-  postPatient : function(req, res) {
-    var Patient = req.body
-      
-    console.log(Patient)
+    console.log(Account);
 
-      var sql = `INSERT INTO biqtzwqiihjmw2npadtd.Patient(Patient_ID, Name, Image, Phone, Email)     VALUES("${Patient.Patient_ID}", "${Patient.Name}", "${Patient.Image}", "${Patient.Phone}", "${Patient.Email}")`
-      database.query(sql, (err, res)=> {
-          if (err)  throw err ;
-          //res.send(Patient)
-      })
+    var sql = `INSERT INTO biqtzwqiihjmw2npadtd.Account(UserName, Password, Role, IsActive, Role_ID)     VALUES("${Account.UserName}", "${Account.Password}", "${Account.Role}", "${Account.IsActive}", "${Account.Role_ID}")`;
+    database.query(sql, (err, res) => {
+      if (err) throw err;
+      res.json(Account);
+    });
   },
-  signin :  function (req, res)  {
+  postPatient: function (req, res) {
+    var Patient = req.body;
+
+    console.log(Patient);
+
+    var sql = `INSERT INTO biqtzwqiihjmw2npadtd.Patient(Patient_ID, Name, Image, Phone, Email)     VALUES("${Patient.Patient_ID}", "${Patient.Name}", "${Patient.Image}", "${Patient.Phone}", "${Patient.Email}")`;
+    database.query(sql, (err, res) => {
+      if (err) throw err;
+      //res.send(Patient)
+    });
+  },
+  signin: function (req, res) {
     const data = req.body;
     console.log(data);
-    const accessToken = jwt.sign(data, "jwtabc", {expiresIn: '60s'});
-    res.json({accessToken});
+    const accessToken = jwt.sign(data, "jwtabc", { expiresIn: "60s" });
+    res.json({ accessToken });
   },
-  getAppointment : function(req, res){
-    var sql = 'SELECT biqtzwqiihjmw2npadtd.Appointment.Appointment_ID, biqtzwqiihjmw2npadtd.Patient.Name, biqtzwqiihjmw2npadtd.Doctor.Name, biqtzwqiihjmw2npadtd.Appointment.DateTime FROM biqtzwqiihjmw2npadtd.Appointment, biqtzwqiihjmw2npadtd.Patient , biqtzwqiihjmw2npadtd.Doctor WHERE biqtzwqiihjmw2npadtd.Appointment.Patient_ID = biqtzwqiihjmw2npadtd.Patient.Patient_ID and biqtzwqiihjmw2npadtd.Appointment.Doctor_ID = biqtzwqiihjmw2npadtd.Doctor.Doctor_ID';
-database.query(sql , (err, response) => {
-    if (err) {
+  getAppointment: function (req, res) {
+    var sql =
+      "SELECT biqtzwqiihjmw2npadtd.Appointment.Appointment_ID, biqtzwqiihjmw2npadtd.Patient.Name, biqtzwqiihjmw2npadtd.Doctor.Name, biqtzwqiihjmw2npadtd.Appointment.DateTime FROM biqtzwqiihjmw2npadtd.Appointment, biqtzwqiihjmw2npadtd.Patient , biqtzwqiihjmw2npadtd.Doctor WHERE biqtzwqiihjmw2npadtd.Appointment.Patient_ID = biqtzwqiihjmw2npadtd.Patient.Patient_ID and biqtzwqiihjmw2npadtd.Appointment.Doctor_ID = biqtzwqiihjmw2npadtd.Doctor.Doctor_ID";
+    database.query(sql, (err, response) => {
+      if (err) {
         console.log("khong the lay du lieu");
-    }
-    res.json(response);
-})
-},
-
-
-}
+      }
+      res.json(response);
+    });
+  },
+  getDoctor: function (req, res) {
+    var sql = `SELECT * FROM biqtzwqiihjmw2npadtd.Doctor`;
+    database.query(sql, (err, response) => {
+      if (err) {
+        console.log("khong lay duoc du lieu");
+      }
+      res.json(response);
+    });
+  },
+};
--- API/route.js
+++ API/route.js
@@ -1,39 +1,39 @@
-const medical_controller = require ("./controller/medical_controller.js")
+const medical_controller = require("./controller/medical_controller.js");
 
-const Role = require("./controller/medical_controller.js")
+const Role = require("./controller/medical_controller.js");
 
-function  authenToken(res, req, next) {
-    //const AuthorizationHeader = req.headers['Authorization'];
-    console.log(res);
-    //console.log(JSON.stringify(req.headers));
-    // const token = authorizationHeader.split(' ')[1];
-    // if (!token) res.sendStatus(401);
+function authenToken(res, req, next) {
+  //const AuthorizationHeader = req.headers['Authorization'];
+  console.log(res);
+  //console.log(JSON.stringify(req.headers));
+  // const token = authorizationHeader.split(' ')[1];
+  // if (!token) res.sendStatus(401);
 
-    // jwt.verify(token, "jwtabc", (err, data) => {
-    //     console.log(err, data);
-    // })
+  // jwt.verify(token, "jwtabc", (err, data) => {
+  //     console.log(err, data);
+  // })
 }
 
-module.exports = function(app) {
+module.exports = function (app) {
+  app.route("/Account").get(authenToken, medical_controller.getAccount);
 
-    app.route("/Account").get(authenToken, medical_controller.getAccount);
+  app.route("/Medicine").get(medical_controller.getMedicine);
 
-    app.route("/Medicine").get(medical_controller.getMedicine);
+  app.route("/user").get(medical_controller.getuser);
 
-    app.route("/user").get(medical_controller.getuser);
+  app.route("/Appointment").get(medical_controller.getAppointment);
 
-    app.route("/Appointment").get(medical_controller.getAppointment);
-    
-    app.route('/Role/:Role_ID').put(Role.update);
+  app.route("/Role/:Role_ID").put(Role.update);
 
-    app.route("/Accounts").post(medical_controller.postAccount);
-        
-    app.route("/Patient").post(medical_controller.postPatient);
+  app.route("/Accounts").post(medical_controller.postAccount);
 
-    //app.route("/signup").post(medical_controller.signup)
+  app.route("/Patient").post(medical_controller.postPatient);
 
-    app.route("/signin").post(medical_controller.signin)
+  //app.route("/signup").post(medical_controller.signup)
 
-    //app.route("/secret").post(medical_controller.secret)
-    };
+  app.route("/signin").post(medical_controller.signin);
 
+  app.route("/doctor").get(medical_controller.getDoctor);
+
+  //app.route("/secret").post(medical_controller.secret)
+};
